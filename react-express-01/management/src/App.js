33477c59d163033de32c7c1161ce1543b40d7ded
Psmin <tkdals6405@gmail.com>
2023-02-07 01:44:23 +0900

    new Toy Project  - React 01

---
+++ react-express-01/management/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+// JSX 문법
+const App = () => {
+  return (
+    <div className="gray-background">
+      <img src={logo} lat="logo" />
+      <h2>Let`s develop management system!</h2>
+    </div>
+  );
+};
+
+export default App;
