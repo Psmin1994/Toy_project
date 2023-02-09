1cdbbf1715d0244d106e0a9979fae1052937c5a1
Psmin <tkdals6405@gmail.com>
2023-02-09 23:03:58 +0900

    Create customer DB & Nodejs DB conn

--- management-react/server/app.js
+++ management-react/server/app.js
@@ -1,17 +1,35 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mysql from "mysql2";
 import dotenv from "dotenv";
+import path from "path";
+const __dirname = path.resolve();
 dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const conn = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+conn.connect();
+
 // 라우터
-app.get("/api/customers", (req, res) => {});
+app.get("/api/customers", (req, res) => {
+  conn.query("SELECT * FROM customers", (err, rows, fields) => {
+    if (err) console.log(err);
+    else res.send(rows);
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server run in ${port}`);
---
+++ management-react/server/src/routes/home/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const express = require("express");
+const router = express.Router();
+
+const ctrl = require("./home.ctrl");
+
+router.get("/", ctrl.output.home);
+router.get("/login", ctrl.output.login);
+router.get("/register", ctrl.output.register);
+
+router.post("/login", ctrl.process.login);
+router.post("/register", ctrl.process.register);
+
+module.exports = router; // 외부로 내보내기 , 사용할 수 있도록 던지기
